4dcbf2d1764026079d5cb06bdb75132aba7df032
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-09-22 14:30:05 +0700

    Add login page

--- src/main/java/th/ac/ku/atm/controller/CustomerService.java
+++ src/main/java/th/ac/ku/atm/controller/CustomerService.java
@@ -26,4 +26,22 @@ public class CustomerService {
         String salt = BCrypt.gensalt(12);
         return BCrypt.hashpw(pin, salt);
     }
+
+    public Customer findCustomer(int id) {
+        for (Customer customer : customers) {
+            if (customer.getId() == id)
+                return customer;
+        }
+        return null;
+    }
+
+    public Customer checkPin(Customer inputCustomer) {
+        Customer storedCustomer = findCustomer(inputCustomer.getId());
+        if (storedCustomer != null) {
+            String hashPin = storedCustomer.getPin();
+            if (BCrypt.checkpw(inputCustomer.getPin(), hashPin))
+                return storedCustomer;
+        }
+        return null;
+    }
 }
