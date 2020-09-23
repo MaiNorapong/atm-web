1cd152b82f37533dd6348e3e13d92272d8585db3
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-09-23 21:16:54 +0700

    Rearrange method

--- src/main/java/th/ac/ku/atm/controller/CustomerService.java
+++ src/main/java/th/ac/ku/atm/controller/CustomerService.java
@@ -28,17 +28,17 @@ public class CustomerService {
         return repository.findAll();
     }
 
-    private String hash(String pin) {
-        String salt = BCrypt.gensalt(12);
-        return BCrypt.hashpw(pin, salt);
-    }
-
     public Customer findCustomer(int id) {
         try {
             return repository.findById(id);
         } catch (EmptyResultDataAccessException e) {
             return null;
         }
+    }
+
+    private String hash(String pin) {
+        String salt = BCrypt.gensalt(12);
+        return BCrypt.hashpw(pin, salt);
     }
 
     public Customer checkPin(Customer inputCustomer) {
