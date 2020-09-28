8d4fc0a0a03ad8bfe9a2be8532e83be904e4ec96
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-09-28 20:56:03 +0700

    Add CustomerRestController

---
+++ src/main/java/th/ac/ku/atm/controller/CustomerRestController.java
@@ -0,0 +1,27 @@
+package th.ac.ku.atm.controller;
+
+import org.springframework.web.bind.annotation.*;
+import th.ac.ku.atm.model.Customer;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/customer")
+public class CustomerRestController {
+
+    private CustomerService customerService;
+
+    public CustomerRestController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    public List<Customer> getAll(){
+        return customerService.getCustomers();
+    }
+
+    @GetMapping("/{id}")
+    public Customer getOne(@PathVariable int id){
+        return customerService.findCustomer(id);
+    }
+}
